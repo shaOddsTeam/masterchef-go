b12f3bbaad63032cc2908eedaa06946bda3f8e36
SHA <sha@odds.team>
2021-07-17 22:54:51 +0700

    add more smart contract

--- Makefile
+++ Makefile
@@ -4,8 +4,23 @@ PWD ?= pwd_unknown
 compile-masterchef: 
 	docker run --rm -v $(PWD):/root ethereum/solc:0.6.12 --abi --bin /root/contracts/MasterChef.sol -o /root/build
 
+compile-twindex-swap-router: 
+	docker run --rm -v $(PWD):/root ethereum/solc:0.6.12 --abi --bin /root/contracts/TwindexSwapRouter.sol -o /root/build-twindex
+
+compile-pancake-swap-router: 
+	docker run --rm -v $(PWD):/root ethereum/solc:0.6.6 --abi --bin /root/contracts/PancakeRouter.sol -o /root/build-pancake-router
+
 abigen-masterchef:
 	mkdir -p masterchef && abigen --bin=./build/MasterChef.bin --abi=./build/MasterChef.abi --pkg=masterchef --out=./masterchef/master-chef.go
 
 abigen-bep20:
 	mkdir -p bep20 && abigen --bin=./build/BEP20.bin --abi=./build/BEP20.abi --pkg=bep20 --out=./bep20/bep20.go
+
+abigen-chainlink:
+	mkdir -p chainlink && abigen --bin=./build-chainlink/EACAggregatorProxy.bin --abi=./build-chainlink/EACAggregatorProxy.abi --pkg=chainlink --out=./chainlink/chainlink.go
+
+abigen-twindex-swap-router:
+	mkdir -p twindex && abigen --bin=./build-twindex/TwindexSwapRouter.bin --abi=./build-twindex/TwindexSwapRouter.abi --pkg=twindex --out=./twindex/twindex.go
+
+abigen-pancake-swap-router:
+	mkdir -p pancake && abigen --bin=./build-pancake-router/PancakeRouter.bin --abi=./build-pancake-router/PancakeRouter.abi --pkg=pancake --out=./pancake/pancake-router.go
--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"log"
 	"math/big"
 
-	bep20 "github.com/atchapcyp/masterchef-go/bep20"
-	masterchef "github.com/atchapcyp/masterchef-go/masterchef"
+	"github.com/atchapcyp/masterchef-go/pancake"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -18,49 +17,65 @@ func main() {
 		log.Fatal(err)
 	}
 
+	contractAddress := common.HexToAddress("0x10ED43C718714eb63d5aA57B78B54704E256024E")
+	instance, err := pancake.NewPancake(contractAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := make([]common.Address, 0)
+	path = append(path, common.HexToAddress("0x55d398326f99059ff775485246999027b3197955"))
+	path = append(path, common.HexToAddress("0x39f1014a88c8ec087cedf1bfc7064d24f507b894"))
+
+	response, err := instance.GetAmountsOut(&bind.CallOpts{}, new(big.Int).SetUint64(100000000), path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("response : %+v\n", response)
+
 	//MASTER CHEF
 
-	contractAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
-	instance, err := masterchef.NewMasterchef(contractAddress, client)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// contractAddress := common.HexToAddress("0x73feaa1eE314F8c655E354234017bE2193C9E24E")
+	// instance, err := masterchef.NewMasterchef(contractAddress, client)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	devAddr, err := instance.Devaddr(&bind.CallOpts{})
-	if err != nil {
+	// devAddr, err := instance.Devaddr(&bind.CallOpts{})
+	// if err != nil {
 
-		log.Fatal(err)
-	}
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Printf("devAddr %+v\n", devAddr)
+	// fmt.Printf("devAddr %+v\n", devAddr)
 
-	poolInfo, err := instance.PoolInfo(&bind.CallOpts{}, big.NewInt(100))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// poolInfo, err := instance.PoolInfo(&bind.CallOpts{}, big.NewInt(100))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Printf("poolinfo %+v\n", poolInfo)
+	// fmt.Printf("poolinfo %+v\n", poolInfo)
 
-	// BEP20
-	bep20ContractAddress := common.HexToAddress("0x55d398326f99059fF775485246999027B3197955")
-	bep20contract, err := bep20.NewBep20(bep20ContractAddress, client)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // BEP20
+	// bep20ContractAddress := common.HexToAddress("0x55d398326f99059fF775485246999027B3197955")
+	// bep20contract, err := bep20.NewBep20(bep20ContractAddress, client)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	tokenName, err := bep20contract.Name(&bind.CallOpts{})
-	if err != nil {
+	// tokenName, err := bep20contract.Name(&bind.CallOpts{})
+	// if err != nil {
 
-		log.Fatal(err)
-	}
+	// 	log.Fatal(err)
+	// }
 
-	fmt.Printf("tokenName : %+v\n", tokenName)
+	// fmt.Printf("tokenName : %+v\n", tokenName)
 
-	tts, err := bep20contract.TotalSupply(&bind.CallOpts{})
-	if err != nil {
+	// tts, err := bep20contract.TotalSupply(&bind.CallOpts{})
+	// if err != nil {
 
-		log.Fatal(err)
-	}
-	fmt.Printf("totalSupply : %+v\n", tts)
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("totalSupply : %+v\n", tts)
 
 }
